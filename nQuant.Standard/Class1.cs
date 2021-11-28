23a0ad397cd1f376bc78cc25c7bcfa3e4b112158
Svyatoslav Razmyslov <razmyslov@ad.viva64.com>
2021-11-28 10:51:23 +0300

    Added .NET Standard project.

---
+++ nQuant.Standard/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace nQuant.Standard
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ nQuant.Standard/QuantizationException.cs
@@ -0,0 +1,8 @@
+﻿namespace nQuant.Standard
+{
+    [System.Serializable]
+    public class QuantizationException : System.ApplicationException
+    {
+        public QuantizationException(string message) : base(message) { }
+    }
+}
